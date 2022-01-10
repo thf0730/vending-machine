14e9dcb099890a3987e056450fc38a12f40a9ab1
thf0730 <thf0730@daum.net>
2022-01-11 02:39:13 +0900

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -117,37 +117,6 @@ void Widget::on_pbReset_clicked()
 }
 
 
-/*
-int quot500, quot100, quot50, quot10; //quotient : 몫
-int rema500, rema100, rema50; //remainder : 나머지
-QString strmoney; //잔돈 money 문자열 변환
-QString strquot500, strquot100, strquot50, strquot10; //string+quotient : 몫 문자열 변환
 
-void Widget::on_pbReset_clicked()
-{
-
-    quot500 = money / 500; //500 배수 갯수
-    rema500 = money % 500; //500 배수 제외 나머지
-
-    quot100 = rema500 / 100; //100 배수 갯수
-    rema100 = money % 100; //100 배수 제외 나머지
-
-    quot50 = rema100 / 50; //50 배수 갯수
-    rema50 = money % 50; //50 배수 제외 나머지
-
-    quot10 = rema50 / 10; //10 배수 갯수
-
-    strmoney = QString::number(money);
-    strquot500 = QString::number(quot500);
-    strquot100 = QString::number(quot100);
-    strquot50 = QString::number(quot50);
-    strquot10 = QString::number(quot10);
-
-    QMessageBox::information(this, "반환", "남은 잔돈은 " + strmoney + "원입니다.\n\n" + "500원 " + strquot500 + "개, " + "100원 " + strquot100 + "개, " + "50원 " + strquot50 + "개, " + "10원 " + strquot10 + "개를 반환합니다.\n");
-
-    changeMoney(-money);
-
-}
-*/
 
 
